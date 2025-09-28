27c68f274b934f17cf2ffbbe8527d82d26e0e1b1
Manish Jha <jhas67761@gmail.com>
2025-09-28 12:58:18 +0530

    Update Merger_Sorted_Array.java

---
+++ ICP_HomeAssign2/Merger_Sorted_Array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int t =  m  + n - 1;
+        int p1 = m-1  , p2 = n-1;
+
+        while(p1 >= 0 && p2 >= 0){ 
+         if(nums1[p1] >= nums2[p2]){
+            nums1[t] = nums1[p1] ;
+            t -- ;
+            p1 -- ;
+         }
+        else{
+          nums1[t] = nums2[p2] ;
+            t -- ;
+            p2 -- ;
+        } 
+        }
+
+        while(p2  >= 0){
+            nums1[t] = nums2[p2] ;
+            t -- ;
+            p2 -- ;
+        }
+
+    }
+}
