0e5992a2a4678d6781e4140221e0ba59f59d2836
Manish Jha <jhas67761@gmail.com>
2025-11-13 09:29:11 +0530

    Update minimum_Arrow_Burst_Ballon.java

---
+++ HomeAssign_5/minimum_Arrow_Burst_Ballon.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+       Arrays.sort(points, (a,b)-> Integer.compare(a[1],b[1])); 
+       int ans  = 1;
+       int a  = points[0][0];
+       int b = points[0][1];
+
+       for(int i=1; i<points.length; i++)
+       {
+          if(b >= points[i][0]) continue;  // overlap 
+          else
+          {
+            ans ++ ;
+            b =  points[i][1]; 
+          }
+       }
+       return ans ; 
+    
+    
+    }
+}
